db20c039a577abc7a2f6c7e75cc13ee66634de4c
HuangZL20231010 <HuangZL1010@aliyun.com>
2023-06-13 18:43:13 +0800

    111

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
+  onLaunch() {
+    const logs = wx.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync("logss", logs);
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success: (res) => {
+        console.log(res)
+        console.log("\u7528\u6237\u7684code:" + res.code);
+        wx.request({
+          method: "POST",
+          url: getApp().globalData.url + "user/login",
+          data: {
+            code: res.code
+          },
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          success: (res2) => {
+            console.log("token : " + res2.data.data.token);
+            this.globalData.token = res2.data.data.token;
+            // let ismatch = res2.data.data.ismatch;
+            // if (ismatch === "1")
+            //   this.globalData.ismatch = true;
+            // else if (ismatch === "0")
+            //   this.globalData.ismatch = false;
+            wx.setStorageSync("token", res2.data.data.token);
+          }
+        });
       }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    });
+  },
+  onShow: function() {
+    console.log("App Show");
+  },
+  onHide: function() {
+    console.log("App Hide");
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    url: "https://localhost:8080/",
+    token: null,
   }
 })
--- pages/me/me.js
+++ pages/me/me.js
@@ -2,11 +2,27 @@
 Page({
 
     data: {
-        
+        contentNewsList: []
     },
 
+    //跳转到用户信息页
+    tapUserInfo() {
+        console.log("[[tapUserInfo]]");
+        getApp().globalData.userBaseInfo = this.userBaseInfo;
+        wx.navigateTo({
+          url: "/pages/userInfo/userInfo"
+        });
+    },
 
-
+    //跳转到新闻详情页
+  viewDetail: function(e) {
+    let newsUrl = e.currentTarget.dataset.newsurl || ''
+    let newsTitle = e.currentTarget.dataset.newstitle || ''
+    let newsAuthor = e.currentTarget.dataset.newsauthor || ''
+    wx.navigateTo({
+      url: '../detail/detail?newsUrl=' + newsUrl
+    })
+  },
 
 
     onLoad(options) {
